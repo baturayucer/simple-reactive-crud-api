30743784f387fda3d9abe329a179aa769d555e23
baturayucer <baturayucer@gmail.com>
2020-05-24 17:13:24 +0300

    add getAllItems Endpoint

---
+++ simple-reactive-client/src/main/java/com/baturayucer/reactiveclient/dto/ItemDto.java
@@ -0,0 +1,13 @@
+package com.baturayucer.reactiveclient.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private String id;
+    private String description;
+    private Double price;
+}
