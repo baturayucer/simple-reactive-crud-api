56261ddfc60187a4e99a7481fd31eea8b9d76774
baturayucer <baturayucer@gmail.com>
2020-05-25 20:04:06 +0300

    add client endpoints

--- simple-reactive-client/src/main/java/com/baturayucer/reactiveclient/constant/ReactiveClientConstants.java
+++ simple-reactive-client/src/main/java/com/baturayucer/reactiveclient/constant/ReactiveClientConstants.java
@@ -7,7 +7,13 @@ public class ReactiveClientConstants {
 
     public static final String API_URL = "http://localhost:8080";
     public static final String REACTIVE_CLIENT_V1 = "/reactiveClient/v1";
+    public static final String LEGACY_CONTROLLER_V1 = "/legacyController/v1";
     public static final String ITEMS_ALL = "/items/all";
+    public static final String ID = "id";
+    public static final String DESCRIPTION = "description";
+    public static final String FIND_ONE = "/items/findOne";
+    public static final String FIND_BY_DESC = "/items/findByDesc";
+    public static final String CREATE_ITEM = "/createItem";
 
     private ReactiveClientConstants() throws IllegalAccessException {
         throw new IllegalAccessException("This is a constant class.");
---
+++ simple-reactive-client/src/main/java/com/baturayucer/reactiveclient/controller/ReactiveClientControllerImpl.java
@@ -0,0 +1,63 @@
+package com.baturayucer.reactiveclient.controller;
+
+import com.baturayucer.reactiveclient.dto.ItemDto;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static com.baturayucer.reactiveclient.constant.ReactiveClientConstants.*;
+
+/**
+ * @author baturayucer.
+ */
+@RestController
+public class ReactiveClientControllerImpl implements ReactiveClientController{
+
+    WebClient webClient = WebClient.create(API_URL);
+
+    public ResponseEntity<Flux<ItemDto>> getAllItems() {
+        Flux<ItemDto> itemDtoFlux = webClient.get()
+                .uri(LEGACY_CONTROLLER_V1 + ITEMS_ALL)
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToFlux(ItemDto.class).log();
+        return ResponseEntity.ok(itemDtoFlux);
+    }
+
+    public ResponseEntity<Mono<ItemDto>> findOne(@RequestParam(value = ID) String id) {
+        Mono<ItemDto> itemDtoMono = webClient.get()
+                .uri(LEGACY_CONTROLLER_V1 + FIND_ONE)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(ID, id)
+                .retrieve()
+                .bodyToMono(ItemDto.class).log();
+        return ResponseEntity.ok(itemDtoMono);
+    }
+
+    public ResponseEntity<Flux<ItemDto>> findByDescription(@RequestParam(value = DESCRIPTION) String description) {
+        Flux<ItemDto> itemDtoFlux = webClient.get()
+                .uri(LEGACY_CONTROLLER_V1 + FIND_BY_DESC)
+                .accept(MediaType.APPLICATION_JSON)
+                .header(DESCRIPTION, description)
+                .retrieve()
+                .bodyToFlux(ItemDto.class).log();
+        return ResponseEntity.ok(itemDtoFlux);
+    }
+
+    public ResponseEntity<Mono<ItemDto>> createItem(@RequestBody ItemDto itemRequest) {
+        Mono<ItemDto> itemDtoFlux = webClient.post()
+                .uri(LEGACY_CONTROLLER_V1 + CREATE_ITEM)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromPublisher(Mono
+                        .just(itemRequest), ItemDto.class))
+                .retrieve()
+                .bodyToMono(ItemDto.class).log();
+        return ResponseEntity.ok(itemDtoFlux);
+    }
+}
